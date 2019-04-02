05b7ec4d23927b7e06aa1adeefe29fe656b6ed9a
Sebastian Fiorini <sebf465@gmail.com>
2019-04-01 21:01:41 -0400

    Initial Commit

---
+++ chip8em/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chip8em
+//
+//  Created by Sebastian Fiorini on 2019-04-01.
+//  Copyright © 2019 Sebastian Fiorini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
